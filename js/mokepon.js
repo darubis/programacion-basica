aea5c3b9ad45266eeba6899ad804e69d2ef229a5
darubis <yennidaruby@gmail.com>
2024-10-08 17:48:52 -0500

    addeventlistener

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,10 +1,22 @@
 function iniciarJuego(){
-    let botonMascotaJugador = document.getElementById("boton-mascota");
+    let botonMascotaJugador = document.getElementById('boton-mascota');
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
 }
 
 function seleccionarMascotaJugador(){
-    alert("Seleccionaste tu mascota")
+    let inputHipodoge = document.getElementById('hipodoge');
+    let inputCapipepo = document.getElementById('capipepo');
+    let inputRatigueya = document.getElementById('ratigueya');
+
+    if(inputHipodoge.checked){
+        alert('Seleccionaste a hipodoge')
+    }else if(inputCapipepo.checked){
+        alert('Seleccionaste a capipepo')
+    }else if(inputRatigueya.checked){
+        alert('Seleccionaste a ratigueya')
+    }else{
+        
+    }
 }
 
 
