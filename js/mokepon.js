280ea146afaf8ea3510c60ae7ab4ce0c41d7a843
darubis <yennidaruby@gmail.com>
2024-10-10 16:56:34 -0500

    Reiniciando el juego

--- js/mokepon.js
+++ js/mokepon.js
@@ -15,6 +15,9 @@ function iniciarJuego(){
 
     let botonTierra = document.getElementById('boton-tierra');
     botonTierra.addEventListener('click', ataqueTierra);
+
+    let botonReiniciar = document.getElementById('boton-reiniciar');
+    botonReiniciar.addEventListener('click', reiniciarJuego);
 }
 
 
@@ -140,6 +143,20 @@ function crearMensajeFinal(resultadoFinal){
     parrafo.innerHTML = resultadoFinal;
 
     sectionMensajes.appendChild(parrafo);
+
+    let botonFuego = document.getElementById('boton-fuego');
+    botonFuego.disabled = true;
+
+    let botonAgua = document.getElementById('boton-agua');
+    botonAgua.disabled = true;
+
+    let botonTierra = document.getElementById('boton-tierra');
+    botonTierra.disabled = true;
+}
+
+
+function reiniciarJuego(){
+    location.reload();
 }
 
 function aleatorio(min, max){
